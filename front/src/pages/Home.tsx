aaf3333985938465e9c2e3b4e90ce1cf62b2a793
AnnkoATAMA <annkopan0903@gmail.com>
2025-01-25 19:39:48 +0900

    横スク対処

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import RaceList from "../components/RaceList.tsx";
-import "./style.css"
 
 const Home = () => {
     return (
-        <div className="home">
+        <div>
             <RaceList />
         </div>
     )
