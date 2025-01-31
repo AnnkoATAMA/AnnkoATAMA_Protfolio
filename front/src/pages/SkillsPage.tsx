0094168386783859bf22d1560b574f9f0a472ff6
AnnkoATAMA <annkopan0903@gmail.com>
2025-01-31 22:33:21 +0900

    skillハイパー簡易実装

--- front/src/pages/SkillsPage.tsx
+++ front/src/pages/SkillsPage.tsx
@@ -1,7 +1,27 @@
+import "./css/skills.css"
+
 const SkillsPage = () => {
   return (
-      <div>
-          skills page
+      <div className={"skill-container"}>
+          <div className={"skill-header"}>
+              血統(スキル早見表)
+          </div>
+          <div className="pedigree-chart">
+              <div className="sire">
+                  <div className="parent">react</div>
+                  <div className="grandparents">
+                      <div className="grandparent">Swift</div>
+                      <div className="grandparent">Vue</div>
+                  </div>
+              </div>
+              <div className="dam">
+                  <div className="parent">Java</div>
+                  <div className="grandparents">
+                      <div className="grandparent">Python</div>
+                      <div className="grandparent">MySQL</div>
+                  </div>
+              </div>
+          </div>
       </div>
   )
 }
